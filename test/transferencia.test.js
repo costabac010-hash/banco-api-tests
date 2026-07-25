77ee4586efbad97b58261626596f4dbd0c02a95c
costabac010-hash <costa.bac010@gmail.com>
2026-07-25 13:57:58 -0300

    Adicionando fixtures para gerir os corpos dad requisicoes

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const posTransferencias = require('../fixtures/posTransferencias.json');
 
 describe('Transferencias', () => {
     describe('POST/transferencias', () => {
@@ -12,23 +13,29 @@ describe('Transferencias', () => {
         });
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async () => {
+            const bodyTransferencias = { ...posTransferencias };
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', 'Bearer ' + token)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                });
-            expect(resposta.status).to.equal(201);
+                .send(bodyTransferencias);
 
+            expect(resposta.status).to.equal(201);
             console.log(resposta.body);
 
-            //it('Deve retornar falha com 422 quando o valor da trasnferencia for abaixo de 10 reais', async () => {
+        });
 
+            it('Deve retornar falha com 422 quando o valor da trasnferencia for abaixo de 10 reais', async () => {
+                const bodyTransferencias = { ...posTransferencias, valor: 9 };
+                const resposta = await request(process.env.BASE_URL)
+                    .post('/transferencias')
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer ' + token)
+                    .send(bodyTransferencias);
 
+                expect(resposta.status).to.equal(422);
+                console.log(resposta.body);
+
+            });
         });
     });
-});
